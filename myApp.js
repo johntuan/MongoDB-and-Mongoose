a935c3d8261adda4ebd2ab663846f6c47ccda459
John <john_ha80@hotmail.com>
2021-05-28 15:41:59 -0400

    Update to credentials for MongoDB

--- myApp.js
+++ myApp.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-
-require('dotenv').config();
-
+const dotenv = require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
