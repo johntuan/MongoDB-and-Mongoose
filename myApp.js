a1a21d31931eef15ac512a30ea48f07f7c827ed2
John <john_ha80@hotmail.com>
2021-05-28 14:06:07 -0400

    MongoDB and Mongoose

--- myApp.js
+++ myApp.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 // require('dotenv').config();
 
 
-mongoose.connect (process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//trying to find out password for mongodb 
 
 // let Person;
 
