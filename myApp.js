23b5c08d16b9a0e50c83de0837a4aa2fd9490744
John <john_ha80@hotmail.com>
2021-05-29 13:23:52 -0400

    MongoDB and Mongoose

--- myApp.js
+++ myApp.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
 //trying to find out password for mongodb 
 
 // let Person;
